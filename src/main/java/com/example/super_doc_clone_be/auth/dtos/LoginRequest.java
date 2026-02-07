2a8eef3d5e74a22091c27a98a3cf08bdabe13001
Icaka <ico.ezekiev@gmail.com>
2026-02-07 19:15:59 +0200

    login first steps

---
+++ src/main/java/com/example/super_doc_clone_be/auth/dtos/LoginRequest.java
@@ -0,0 +1,4 @@
+package com.example.super_doc_clone_be.auth.dtos;
+
+public record LoginRequest(String email, String password) {
+}
--- src/main/java/com/example/super_doc_clone_be/config/SecurityConfig.java
+++ src/main/java/com/example/super_doc_clone_be/config/SecurityConfig.java
@@ -3,8 +3,11 @@ package com.example.super_doc_clone_be.config;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,11 +21,11 @@ public class SecurityConfig {
 
         http
                 // API → no CSRF
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
 
                 // No Spring login page
-                .formLogin(form -> form.disable())
-                .httpBasic(basic -> basic.disable())
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
 
                 // Authorization rules
                 .authorizeHttpRequests(auth -> auth
@@ -46,5 +49,11 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
