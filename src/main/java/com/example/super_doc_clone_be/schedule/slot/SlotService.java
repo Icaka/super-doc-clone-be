2968802e0696d25186df808eee47686ce03bca0f
Icaka <ico.ezekiev@gmail.com>
2026-03-05 22:17:48 +0200

    blocking a slot cancels appointment

--- src/main/java/com/example/super_doc_clone_be/schedule/slot/SlotService.java
+++ src/main/java/com/example/super_doc_clone_be/schedule/slot/SlotService.java
@@ -1,10 +1,14 @@
 package com.example.super_doc_clone_be.schedule.slot;
 
+import com.example.super_doc_clone_be.appointment.Appointment;
+import com.example.super_doc_clone_be.appointment.AppointmentRepository;
+import com.example.super_doc_clone_be.appointment.AppointmentStatus;
 import com.example.super_doc_clone_be.schedule.ScheduleRepository;
 import com.example.super_doc_clone_be.security.CurrentUserService;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -12,11 +16,13 @@ public class SlotService {
     private final ScheduleRepository scheduleRepository;
     private final SlotRepository slotRepository;
     private final CurrentUserService currentUserService;
+    private final AppointmentRepository appointmentRepository;
 
-    SlotService(ScheduleRepository scheduleRepository, SlotRepository slotRepository, CurrentUserService currentUserService) {
+    SlotService(ScheduleRepository scheduleRepository, SlotRepository slotRepository, CurrentUserService currentUserService, AppointmentRepository appointmentRepository) {
         this.scheduleRepository = scheduleRepository;
         this.slotRepository = slotRepository;
         this.currentUserService = currentUserService;
+        this.appointmentRepository = appointmentRepository;
     }
 
     public void defaultSlotCreation(Integer scheduleId, LocalTime workStart, Integer slotLength, Integer slotCount) {
@@ -38,8 +44,21 @@ public class SlotService {
         }
         Slot temp = slotRepository.findById(slot_id)
                 .orElseThrow(() -> new RuntimeException("Slot with this id doesn't exist in db"));
-        temp.setStatus(SlotStatus.BLOCKED); //TODO what happens if slot is booked
+        temp.setStatus(SlotStatus.BLOCKED);
+        cancelAppointment(slot_id);
         slotRepository.save(temp);
         return true;
+    }
+
+    public void cancelAppointment(Integer slotId) {
+        //TODO maybe move logic to AppointmentService
+        List<Appointment> temp = this.appointmentRepository.findBySlot_Id(slotId);
+        if (temp.isEmpty()) {
+            return;
+        }
+        Appointment tempAppointment = temp.getFirst();
+        tempAppointment.setSlot(null);
+        tempAppointment.setStatus(AppointmentStatus.CANCELLED);
+        this.appointmentRepository.save(tempAppointment);
     }
 }
